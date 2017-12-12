940dd54d526785c48e76472b4719038c7a70109b
schemr <schemryj@gmail.com>
2017-12-12 17:22:31 +0900

    Change real DB fetch and async await logic

--- src/App.js
+++ src/App.js
@@ -2,32 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    title : "Metrix",
-    poster : "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-  },
-  {
-    title : "Full Metal Jacket",
-    poster : "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title : "Oldboy",
-    poster : "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-  },
-  {
-    title : "Star Wars",
-    poster : "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg"
-  }
-]
-
 class App extends Component {
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Update : compoentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {};
+
+  componentWillMount() {
+    this._getMovies();
+  }
+
+  componentDidMount() {
+    
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie key={movie.id} title={movie.title} poster={movie.small_cover_image} />
+    })
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rationg')
+    .then(response => response.json())
+    .then(data => data.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie key={index} title={movie.title} poster={movie.poster} />
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
