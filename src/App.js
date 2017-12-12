b606d42cc6bd30d26841e255ce9a74eed75ac117
schemr <schemryj@gmail.com>
2017-12-12 15:13:33 +0900

    Create movie, poster component test dummy data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
+
+const movies = [
+  {
+    title : "Metrix",
+    poster : "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+  },
+  {
+    title : "Full Metal Jacket",
+    poster : "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+  },
+  {
+    title : "Oldboy",
+    poster : "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
+  },
+  {
+    title : "Star Wars",
+    poster : "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg"
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {movies.map((movie, index) => {
+          return <Movie key={index} title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
